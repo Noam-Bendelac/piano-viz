2288a750572e8d64a767e7fcef167950ce5999e0
Noam Bendelac <bendelac.noam@gmail.com>
2023-05-08 18:14:23 -0400

    Lengthen tempo tail

--- src/render/TempoRenderer.tsx
+++ src/render/TempoRenderer.tsx
@@ -32,7 +32,7 @@ const tempoMat = new ShaderMaterial({
       for (int i = 0; i < ${tempoGpuData.length}; i++) {
         float t = worldPos.x - notes[i].time;
         float p = worldPos.y - notes[i].pitch;
-        sum += notes[i].dTempo * ((t < 0.) ? 0.5*exp(10.*t/1000.) : exp(-2.*t/1000.)) * exp(-0.005*p*p);
+        sum += notes[i].dTempo * ((t < 0.) ? 0.5*exp(7.*t/1000.) : exp(-1.3*t/1000.)) * exp(-0.005*p*p);
       }
       // sum = notes[95].dTempo;
       // sum = worldPos.x/1000.;
